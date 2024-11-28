1f354aefd3ca191778bf58c383da23490bde731d
gayathrikris2012 <gayathrikris2012@gmail.com>
2024-11-28 19:37:51 +0530

    Add files via upload
    week1 assignment

---
+++ assignmentone.js
@@ -0,0 +1,10 @@
+const browserVersion="chrome";
+function getBrowserVersion()
+{
+    if(browserVersion.startsWitj("chrome"))
+       var browserVersion;
+        console.log("Browser version is" + browserVersion);
+        console.log("data type of browser version is" + typeof(browserVersion));  
+     
+}
+getBrowserVersion();
---
+++ datatypes.js
@@ -0,0 +1,14 @@
+let firstName= "Gayathri";
+let companyName  = "Testleaf";
+let mobileNumber=9840360566;
+let isAutomation= true;
+//let hasPlaywright;  // isPlaywright is not defined
+console.log("First name is"+firstName );
+console.log("Data type of first name is" +typeof(firstName));
+console.log("Company name is"+companyName );
+console.log("Data type of company name is" +typeof(companyName));
+console.log("mobileNumber is"+mobileNumber );
+console.log("Data type of mobile number is" +typeof(mobileNumber));
+console.log("isAutomation available or not"+isAutomation );
+console.log("Data type of IsAutomation"+typeof(isAutomation ));
+//console.log(isPlaywright);
